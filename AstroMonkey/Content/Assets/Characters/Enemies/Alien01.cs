da7f3225806b12c3ae54bace678506dfbfede144
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2019-03-22 22:58:53 +0100

    Added alien animation. Changed GameDev Scene

---
+++ AstroMonkey/Content/Assets/Characters/Enemies/Alien01.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace AstroMonkey.Assets.Objects
+{
+    class Alien01: Core.GameObject
+    {
+        public Alien01()
+        {
+            Load(new Core.Transform(Vector2.Zero, Vector2.One, 0f));
+        }
+        public Alien01(Core.Transform _transform)
+        {
+            Load(_transform);
+        }
+        public Alien01(Vector2 position, Vector2 scale, float rotation = 0f)
+        {
+            Load(new Core.Transform(position, scale, rotation));
+        }
+        public Alien01(Vector2 position)
+        {
+            Load(new Core.Transform(position, Vector2.One, 0f));
+        }
+
+        private void Load(Core.Transform _transform)
+        {
+            transform = _transform;
+            AddComponent(new Graphics.Sprite(this, "alien01", new List<Rectangle> {
+                new Rectangle(0, 0, 21, 21),
+                new Rectangle(21, 0, 21, 21),
+                new Rectangle(42, 0, 21, 21),
+                new Rectangle(63, 0, 21, 21),
+                new Rectangle(84, 0, 21, 21),
+                new Rectangle(105, 0, 21, 21),
+                new Rectangle(126, 0, 21, 21),
+                new Rectangle(147, 0, 21, 21),
+                new Rectangle(168, 0, 21, 21),
+                new Rectangle(189, 0, 21, 21),
+                new Rectangle(210, 0, 21, 21),
+                new Rectangle(231, 0, 21, 21),
+                new Rectangle(252, 0, 21, 21),
+                new Rectangle(273, 0, 21, 21),
+                new Rectangle(294, 0, 21, 21),
+                new Rectangle(315, 0, 21, 21),
+                new Rectangle(336, 0, 21, 21),
+                new Rectangle(357, 0, 21, 21),
+                new Rectangle(378, 0, 21, 21),
+                new Rectangle(399, 0, 21, 21),
+                new Rectangle(420, 0, 21, 21)}));
+
+            AddComponent(new Graphics.StackAnimator(this));
+
+            GetComponent<Graphics.StackAnimator>().AddAnimation(
+                new Graphics.StackAnimation("Idle",
+                GetComponent<Graphics.Sprite>(),
+                new List<List<Rectangle>> {
+                    new List<Rectangle> {
+                        new Rectangle(0, 0, 21, 21),
+                        new Rectangle(21, 0, 21, 21),
+                        new Rectangle(42, 0, 21, 21),
+                        new Rectangle(63, 0, 21, 21),
+                        new Rectangle(84, 0, 21, 21),
+                        new Rectangle(105, 0, 21, 21),
+                        new Rectangle(126, 0, 21, 21),
+                        new Rectangle(147, 0, 21, 21),
+                        new Rectangle(168, 0, 21, 21),
+                        new Rectangle(189, 0, 21, 21),
+                        new Rectangle(210, 0, 21, 21),
+                        new Rectangle(231, 0, 21, 21),
+                        new Rectangle(252, 0, 21, 21),
+                        new Rectangle(273, 0, 21, 21),
+                        new Rectangle(294, 0, 21, 21),
+                        new Rectangle(315, 0, 21, 21),
+                        new Rectangle(336, 0, 21, 21),
+                        new Rectangle(357, 0, 21, 21),
+                        new Rectangle(378, 0, 21, 21),
+                        new Rectangle(399, 0, 21, 21),
+                        new Rectangle(420, 0, 21, 21),
+                    },
+                    new List<Rectangle> {
+                        new Rectangle(0, 21, 21, 21),
+                        new Rectangle(21, 21, 21, 21),
+                        new Rectangle(42, 21, 21, 21),
+                        new Rectangle(63, 21, 21, 21),
+                        new Rectangle(84, 21, 21, 21),
+                        new Rectangle(105, 21, 21, 21),
+                        new Rectangle(126, 21, 21, 21),
+                        new Rectangle(147, 21, 21, 21),
+                        new Rectangle(168, 21, 21, 21),
+                        new Rectangle(189, 21, 21, 21),
+                        new Rectangle(210, 21, 21, 21),
+                        new Rectangle(231, 21, 21, 21),
+                        new Rectangle(252, 21, 21, 21),
+                        new Rectangle(273, 21, 21, 21),
+                        new Rectangle(294, 21, 21, 21),
+                        new Rectangle(315, 21, 21, 21),
+                        new Rectangle(336, 21, 21, 21),
+                        new Rectangle(357, 21, 21, 21),
+                        new Rectangle(378, 21, 21, 21),
+                        new Rectangle(399, 21, 21, 21),
+                        new Rectangle(420, 21, 21, 21),
+                    }},
+                266,
+                true));
+
+            GetComponent<Graphics.StackAnimator>().SetAnimation("Idle");
+        }
+    }
+}
--- AstroMonkey/Content/Assets/Scenes/DevRoom.cs
+++ AstroMonkey/Content/Assets/Scenes/DevRoom.cs
@@ -13,16 +13,16 @@ namespace AstroMonkey.Assets.Scenes
         public override void Load()
         {
             //DODAWANIE PODŁOGI
-            for(int x = 10; x < 15; ++x)
+            for(int x = 0; x < 25; ++x)
             {
-                for(int y = 3; y < 10; ++y)
+                for(int y = 0; y < 15; ++y)
                 {
                     objects.Add(new Objects.Floor(new Vector2(x * 32f, y * 32f)));
                     (objects.Last() as Objects.Floor).RandFloor();
                 }
             }
 
-            //DODAWANIE ŚCIAN
+            /*//DODAWANIE ŚCIAN
             for(int x = 10; x < 15; ++x)
             {
                 for(int y = 2; y < 3; ++y)
@@ -40,7 +40,7 @@ namespace AstroMonkey.Assets.Scenes
             }
 
             //INTERAKTYWNE ELEMENY
-            objects.Add(new Objects.Computer(new Vector2(12 * 32f, 2 * 32f + 8f)));
+            objects.Add(new Objects.Computer(new Vector2(12 * 32f, 2 * 32f + 8f)));*/
 
             //DODAWANIE POSTACI
             Objects.Player player = new Objects.Player(new Vector2(20f, 50f));
@@ -48,29 +48,29 @@ namespace AstroMonkey.Assets.Scenes
             Graphics.ViewManager.playerTransform = player.transform;
             objects.Last().transform.scale = new Vector2(3f, 3f);
 
-            /*objects.Add(new Objects.Monkey(new Vector2(350f, 170f)));
+            objects.Add(new Objects.Alien01(new Vector2(350f, 170f)));
             objects.Last().transform.rotation = (float)(Math.PI);
             objects.Last().transform.scale = new Vector2(4f, 4f);
 
-            objects.Add(new Objects.Monkey(new Vector2(420f, 200f)));
+            objects.Add(new Objects.Alien01(new Vector2(420f, 200f)));
             objects.Last().transform.rotation = (float)(Math.PI * 0.5);
             objects.Last().transform.scale = new Vector2(4f, 4f);
 
-            objects.Add(new Objects.Monkey(new Vector2(320f, 280f)));
+            objects.Add(new Objects.Alien01(new Vector2(320f, 280f)));
             objects.Last().transform.rotation = (float)(Math.PI * 1.1);
             objects.Last().transform.scale = new Vector2(4f, 4f);
 
-            objects.Add(new Objects.Monkey(new Vector2(520f, 300f)));
+            objects.Add(new Objects.Alien01(new Vector2(520f, 300f)));
             objects.Last().transform.rotation = (float)(Math.PI * 1.8);
-            objects.Last().transform.scale = new Vector2(4f, 4f);*/
+            objects.Last().transform.scale = new Vector2(4f, 4f);
 
-            objects.Add(new Objects.Klucha(new Vector2(90f, 70f)));
+            /*objects.Add(new Objects.Klucha(new Vector2(90f, 70f)));
             objects.Last().GetComponent<Graphics.Animator>().SetAnimation("WalkRight");
 
             objects.Add(new Objects.Klucha(new Vector2(140f, 100f)));
             objects.Last().GetComponent<Graphics.Animator>().SetAnimation("WalkUp");
 
-            objects.Add(new Objects.Banana(new Vector2(240f, 210f)));
+            objects.Add(new Objects.Banana(new Vector2(240f, 210f)));*/
         }
 
         public override void UnLoad()
