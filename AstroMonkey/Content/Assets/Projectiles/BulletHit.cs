c020640309bafc7c376f45463f758696a13b64af
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2019-03-27 14:02:42 +0100

    Fixed nut gfx. Added bullets gfx.

---
+++ AstroMonkey/Content/Assets/Projectiles/BulletHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace AstroMonkey.Assets.Objects
+{
+    class BulletHit: Core.GameObject
+    {
+        public BulletHit() : this(new Core.Transform())
+        {
+        }
+
+        public BulletHit(Core.Transform _transform) : base(_transform)
+        {
+            Load(_transform);
+        }
+
+        public BulletHit(Vector2 position, Vector2 scale, float rotation = 0f) : this(new Core.Transform(position, scale, rotation))
+        {
+        }
+
+        public BulletHit(Vector2 position) : this(new Core.Transform(position, Vector2.One, 0f))
+        {
+        }
+
+        private void Load(Core.Transform _transform)
+        {
+            AddComponent(new Graphics.Sprite(this, "BulletHit", new List<Rectangle>() { new Rectangle(0, 0, 3, 3) }));
+        }
+    }
+}
--- AstroMonkey/src/Graphics/SpriteContainer.cs
+++ AstroMonkey/src/Graphics/SpriteContainer.cs
@@ -16,21 +16,32 @@ namespace AstroMonkey.Graphics
 
         public void LoadTextures(Game game)
         {
+            //interfejs
             images.Add("mark", game.Content.Load<Texture2D>(@"gfx/UI/HUD/mark"));
 
-            images.Add("player", game.Content.Load<Texture2D>(@"gfx/Characters/Player/PlayerConture"));
+            //efekty
+            images.Add("poop", game.Content.Load<Texture2D>(@"gfx/Projectiles/Poop")); 
+            images.Add("rocket", game.Content.Load<Texture2D>(@"gfx/Projectiles/Rocket"));
+            images.Add("bulletHit", game.Content.Load<Texture2D>(@"gfx/Projectiles/BulletHit"));
+            images.Add("alienBullet", game.Content.Load<Texture2D>(@"gfx/Projectiles/AlienBullet"));
+
+            //postacie
             images.Add("monkey", game.Content.Load<Texture2D>(@"gfx/Characters/Player/monkey"));
-            images.Add("enemy", game.Content.Load<Texture2D>(@"gfx/Characters/Enemies/Klucha"));
             images.Add("alien01", game.Content.Load<Texture2D>(@"gfx/Characters/Enemies/alien01"));
+
+            //otoczenie
             images.Add("floor", game.Content.Load<Texture2D>(@"gfx/Map/Floor/PlatformFloor"));
             images.Add("wall", game.Content.Load<Texture2D>(@"gfx/Map/Objects/wall"));
             images.Add("corner", game.Content.Load<Texture2D>(@"gfx/Map/Objects/corner"));
+            images.Add("computer", game.Content.Load<Texture2D>(@"gfx/Map/Interactable/Computer"));
+
+            //przedmioty
             images.Add("banana", game.Content.Load<Texture2D>(@"gfx/Items/Banana"));
             images.Add("nut", game.Content.Load<Texture2D>(@"gfx/Items/Nut"));
             images.Add("ammoRiffle", game.Content.Load<Texture2D>(@"gfx/Items/AmmoRiffle"));
             images.Add("ammoGun", game.Content.Load<Texture2D>(@"gfx/Items/AmmoGun"));
             images.Add("ammoLuncher", game.Content.Load<Texture2D>(@"gfx/Items/AmmoLuncher"));
-            images.Add("computer", game.Content.Load<Texture2D>(@"gfx/Map/Interactable/Computer"));
+            
         }
 
         public Texture2D GetImage(string name)
