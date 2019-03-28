23a1a8c928a86c81ff1267985ee4ed5e880fcda5
Maciej Nabialczyk 209404@edu.p.lodz.pl <vxd55555@gmail.com>
2019-03-28 19:07:06 +0100

    Added alien03. Refactor animations.

---
+++ AstroMonkey/Content/Assets/Characters/Enemies/Alien03.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace AstroMonkey.Assets.Objects
+{
+    class Alien03: Core.GameObject
+    {
+        public Alien03()
+        {
+            Load(new Core.Transform(Vector2.Zero, Vector2.One, 0f));
+        }
+        public Alien03(Core.Transform _transform) : base(_transform)
+        {
+            Load(_transform);
+        }
+        public Alien03(Vector2 position, Vector2 scale, float rotation = 0f)
+        {
+            Load(new Core.Transform(position, scale, rotation));
+        }
+        public Alien03(Vector2 position)
+        {
+            Load(new Core.Transform(position, Vector2.One, 0f));
+        }
+
+        private int height = 32;
+        private int size = 32;
+
+        private void Load(Core.Transform _transform)
+        {
+            transform = _transform;
+            List<Rectangle> idle01 = new List<Rectangle>();
+            for(int i = 0; i < height; ++i) idle01.Add(new Rectangle(i * size, 0, size, size));
+            AddComponent(new Graphics.Sprite(this, "alien03", idle01));
+
+            AddComponent(new Graphics.StackAnimator(this));
+
+            //STANIE
+            List<Rectangle> idle02 = new List<Rectangle>();
+            for(int i = 0; i < height; ++i) idle02.Add(new Rectangle(i * size, size, size, size));
+            GetComponent<Graphics.StackAnimator>().AddAnimation(
+                new Graphics.StackAnimation("Idle",
+                GetComponent<Graphics.Sprite>(),
+                new List<List<Rectangle>> {
+                    idle01, idle02
+                },
+                266,
+                true));
+
+            //CHODZENIE
+            List<Rectangle> walk01 = new List<Rectangle>();
+            for(int i = 0; i < height; ++i) walk01.Add(new Rectangle(i * size, size * 2, size, size));
+            List<Rectangle> walk02 = new List<Rectangle>();
+            for(int i = 0; i < height; ++i) walk02.Add(new Rectangle(i * size, size * 3, size, size));
+            GetComponent<Graphics.StackAnimator>().AddAnimation(
+                new Graphics.StackAnimation("Walk",
+                GetComponent<Graphics.Sprite>(),
+                new List<List<Rectangle>> {
+                    idle01, walk01, idle01, walk02
+                },
+                182,
+                true));
+
+            GetComponent<Graphics.StackAnimator>().SetAnimation("Idle");
+        }
+    }
+}
