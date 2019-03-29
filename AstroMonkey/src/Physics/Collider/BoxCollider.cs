b27d0db85c2a0ff01ebc8063e7e5c8de34eaad7f
Rootar <mokrosinski95@gmail.com>
2019-03-29 18:54:57 +0100

    Add relative collider position rotation, collider dimensions. Can only be rotate by 90, 180 and 270 deg

--- AstroMonkey/src/Physics/Collider/BoxCollider.cs
+++ AstroMonkey/src/Physics/Collider/BoxCollider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AstroMonkey.Core;
 using Microsoft.Xna.Framework;
 
@@ -11,8 +12,19 @@ namespace AstroMonkey.Physics.Collider
 
         public BoxCollider(GameObject gameObject, CollisionChanell collisionChanell = CollisionChanell.Object, Vector2 relativePosition = new Vector2(), float width = 1, float height = 1) : base(gameObject, collisionChanell, relativePosition)
         {
-            this.width = width * scale;
-            this.height = height * scale;
+            float epsilon = (float)Math.PI / 8;
+            float rotation = Parent.transform.rotation;
+
+            if (Math.Abs(rotation - Math.PI / 2) < epsilon || Math.Abs(rotation + Math.PI / 2) < epsilon)
+            {
+                this.height = width * scale;
+                this.width = height * scale;
+            }
+            else
+            {
+                this.width = width * scale;
+                this.height = height * scale;
+            }
         }
     }
 }
