603521295841784a4b2d979956bc5e0ca9dfda94
Jakub <kuba.jakub17@gmail.com>
2019-03-29 16:45:50 +0100

    Bananers!

--- AstroMonkey/Content/Assets/Scenes/Basement.cs
+++ AstroMonkey/Content/Assets/Scenes/Basement.cs
@@ -8,10 +8,23 @@ namespace AstroMonkey.Assets.Scenes
         public override void Load()
         {
             base.Load();
-            objects.Add(new Objects.Alien01(new Vector2(350f, 170f), new Vector2(sceneScale, sceneScale), (float)(Math.PI)));
+            Vector2 alienPos = new Vector2(350f, 170f);
+            objects.Add(new Objects.Alien01(alienPos, new Vector2(sceneScale, sceneScale), (float)(Math.PI)));
 
-            Objects.Player player = new Objects.Player(new Vector2(20f, 650f), new Vector2(sceneScale, sceneScale), 0f);
+            Vector2 playerPos = new Vector2(20f, 650f);
+
+            Objects.Player player = new Objects.Player(playerPos, new Vector2(sceneScale, sceneScale), 0f);
             objects.Add(player);
+
+            Vector2 step = alienPos - playerPos;
+            step /= 10f;
+            for(int i = 0; i < 9; ++i)
+            {
+                playerPos += step;
+                Core.GameObject gameObject = new Objects.Banana(playerPos, new Vector2(sceneScale, sceneScale));
+                objects.Add(gameObject);
+            }
+
             Graphics.ViewManager.Instance.PlayerTransform = player.transform;
         }
     }
