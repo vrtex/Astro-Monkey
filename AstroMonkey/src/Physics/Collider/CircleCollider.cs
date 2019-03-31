8d1548225d3f62eb3eaf7a29d070ccd4061d3a10
Rootar <mokrosinski95@gmail.com>
2019-03-31 20:50:10 +0200

    Add methods for drawing collider border

--- AstroMonkey/src/Physics/Collider/CircleCollider.cs
+++ AstroMonkey/src/Physics/Collider/CircleCollider.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using AstroMonkey.Core;
+using AstroMonkey.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AstroMonkey.Physics.Collider
 {
@@ -12,6 +17,39 @@ namespace AstroMonkey.Physics.Collider
             : base(gameObject, collisionChanell, relativePosition, temp)
         {
             this.radius = radius * scale;
+        }
+
+        public override void DrawBorder(SpriteBatch spriteBatch)
+        {
+            Color borderColor = Color.Red;
+            int thicknessOfBorder = 1;
+
+            Texture2D pixel;
+            pixel = new Texture2D(spriteBatch.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            pixel.SetData(new[] { Color.White });
+
+            float width = radius * 2;
+            float height = radius * 2;
+
+            // Draw top line
+            spriteBatch.Draw(pixel, new Rectangle((int)(this.GetPosition().X - width / 2), (int)(this.GetPosition().Y - height / 2), (int)width, thicknessOfBorder), borderColor);
+
+            // Draw left line
+            spriteBatch.Draw(pixel, new Rectangle((int)(this.GetPosition().X - width / 2), (int)(this.GetPosition().Y - height / 2), thicknessOfBorder, (int)height), borderColor);
+
+            // Draw right line
+            spriteBatch.Draw(pixel, new Rectangle(((int)(this.GetPosition().X + width / 2) - thicknessOfBorder),
+                (int)(this.GetPosition().Y - height / 2),
+                thicknessOfBorder,
+                (int)height), borderColor);
+
+            // Draw bottom line
+            spriteBatch.Draw(pixel, new Rectangle((int)(this.GetPosition().X - width / 2),
+                (int)(this.GetPosition().Y + height / 2) - thicknessOfBorder,
+                (int)width,
+                thicknessOfBorder), borderColor);
+
+            spriteBatch.DrawString(Graphics.SpriteContainer.Instance.GetFont("text"), this.parent.ToString().Split('.').Last(), GetPosition(), Color.Red);
         }
     }
 }
