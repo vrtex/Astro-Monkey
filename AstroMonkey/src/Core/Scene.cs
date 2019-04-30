a916e24b56f95fd2fe9f16c33b1a591cff538aa1
Jakub <kuba.jakub17@gmail.com>
2019-04-30 22:23:40 +0200

    cleanup

--- AstroMonkey/src/Core/Scene.cs
+++ AstroMonkey/src/Core/Scene.cs
@@ -40,8 +40,6 @@ namespace AstroMonkey.Core
                     Group g2 = m.Groups[2];
                     mapWidth = int.Parse(g1.Value);
                     mapHeight = int.Parse(g2.Value);
-                    Console.WriteLine(mapWidth);
-                    Console.WriteLine(mapHeight);
                 }
 
                 Regex regex = new Regex(@"<data encoding=.csv.>(.*?)<\/data>");
@@ -61,7 +59,6 @@ namespace AstroMonkey.Core
 
                         SpwanUsingTypeIndex(i % mapWidth, i / mapWidth, index);
                     }
-                    // TODO this thing
                 }
             }
             GameManager.FinalizeSpwaning();
