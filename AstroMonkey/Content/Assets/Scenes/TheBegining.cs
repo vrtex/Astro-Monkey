6f7cf5fcee09eb0817040d1a7b757b2792331c5d
Jakub <kuba.jakub17@gmail.com>
2019-06-06 15:36:12 +0200

    Beginning scrolling

--- AstroMonkey/Content/Assets/Scenes/TheBegining.cs
+++ AstroMonkey/Content/Assets/Scenes/TheBegining.cs
@@ -10,10 +10,15 @@ namespace AstroMonkey.Assets.Scenes
 	class TheBegining: Core.Scene
 	{
 		List<Graphics.Widget> scenes			= new List<Graphics.Widget>();
+        Input.ActionBinding progressBinding = new Input.ActionBinding(Input.EMouseButton.Left);
+        private readonly string bindName = "progress";
 
 		public override void Load()
 		{
 			base.Load();
+
+            Input.InputManager.Manager.AddActionBinding(bindName, progressBinding);
+            progressBinding.OnTrigger += ProgressScenes;
 
 			for(int i = 5; i >= 1; --i)
 			{
@@ -23,5 +28,30 @@ namespace AstroMonkey.Assets.Scenes
 
 			Graphics.WidgetManager.AddWidget(scenes.Last());
 		}
-	}
+
+        private void ProgressScenes()
+        {
+            var scene = scenes.Last();
+            Graphics.WidgetManager.RemoveWidget(scene);
+
+            scenes.Remove(scene);
+
+            if(scenes.Count == 0)
+            {
+                Core.GameManager.Instance.NextScene = "level1";
+            }
+            else
+            {
+                Graphics.WidgetManager.AddWidget(scenes.Last());
+            }
+        }
+
+        public override void UnLoad()
+        {
+            scenes.Clear();
+            Input.InputManager.Manager.RemoveBinding(bindName);
+
+            base.UnLoad();
+        }
+    }
 }
